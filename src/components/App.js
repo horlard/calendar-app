6c61813d8fb8046191aebf441b9b54044dad1e86
horlard <oladipuposaulih@gmail.com>
2019-07-24 18:56:21 +0100

    imported eventCalendar component

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import EventCalender from './eventCalender';
+
+
+class App extends React.Component {
+    
+
+
+    render() {
+        return (
+            <div>
+                <EventCalender />
+            </div>
+        )
+    }
+} 
+
+export default App;
