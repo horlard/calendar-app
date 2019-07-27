87e81df0eeae9c000e5199f46da3588a2692880e
horlard <oladipuposaulih@gmail.com>
2019-07-27 14:14:20 +0100

    implemented the provider functionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
+import Reducers from './reducers/combineReducers';
 import App from './components/App';
 
 
 ReactDOM.render(
-        <App />,
+    <Provider store={createStore(Reducers)}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 )
 
