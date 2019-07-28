d62620c0d95e3d557030b75810c5c5a8ac9e963e
horlard <oladipuposaulih@gmail.com>
2019-07-28 02:26:44 +0100

    initialized redux dev-tools

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-
+import {createStore,applyMiddleware,compose} from 'redux';
+import thunk from 'redux-thunk';
 import Reducers from './reducers/combineReducers';
-import App from './components/App';
+import App from '../src/components/App';
 
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store= createStore(Reducers,composeEnhancers(applyMiddleware(thunk)));
 ReactDOM.render(
-    <Provider store={createStore(Reducers)}>
+    <Provider store={store}>
         <App />
     </Provider>,
     document.querySelector('#root')
