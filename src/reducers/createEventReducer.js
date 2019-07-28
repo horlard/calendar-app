01f84128a25a43eb172152b576936408a911392a
horlard <oladipuposaulih@gmail.com>
2019-07-28 02:40:00 +0100

    update

--- src/reducers/createEventReducer.js
+++ src/reducers/createEventReducer.js
@@ -1,6 +1,6 @@
 export default (state={},action) => {
     if(action.type==='EVENT_CREATE') {
-        return {state,...action.payload}
+        return {...action.payload}
     }
     return state;
 }
