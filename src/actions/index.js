1a2b1a888aa96fe04bf1c3a4c7d7953237b922d5
horlard <oladipuposaulih@gmail.com>
2019-07-29 15:34:33 +0100

    made redux handle programmatic navigation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
-
-export const eventCreate = formValues => {
-    return {
-        type : 'EVENT_CREATE',
-        payload : formValues
-    }
+import History from '../components/history';
+export const eventCreate = formValues => dispatch => {
+    dispatch({
+            type : 'EVENT_CREATE',
+            payload : formValues
+    })
+    History.push('/')
+    
     
 }
